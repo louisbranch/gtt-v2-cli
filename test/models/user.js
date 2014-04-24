c612cca6c58ae40e7b04dd8fd483008ac95aa57b
Luiz Branco <me@luizbranco.com>
2014-04-24 00:34:16 -0300

    Add test for user model

---
+++ test/models/user.js
@@ -0,0 +1,68 @@
+var co = require("co");
+var assert = require("assert");
+var nock = require("nock");
+var model = require("../../api/models/user");
+
+describe("user model", function(){
+
+  describe("signup", function(){
+
+    var credentials = {
+      email: "me@luizbranco.com",
+      password: "secret",
+      user: false
+    };
+
+    before(function(){
+      nock("http://localhost:8080/")
+        .post("/v1/signup?email=me@luizbranco.com&password=secret")
+        .reply(200, "12345");
+    });
+
+    it("returns user info", function(done){
+      co(function* () {
+        var response = yield model.authenticate(credentials);
+        assert.deepEqual({
+          token: "12345",
+          email: "me@luizbranco.com",
+          projects: []
+        }, response);
+        done();
+      })();
+    });
+
+  });
+
+  describe("login", function(){
+
+    var credentials = {
+      email: "me@luizbranco.com",
+      password: "secret",
+      user: true
+    };
+
+    before(function(){
+      nock("http://localhost:8080/")
+        .post("/v1/login?email=me@luizbranco.com&password=secret")
+        .reply(200, {
+          token: "12345",
+          email: "me@luizbranco.com",
+          projects: ["test"]
+        });
+    });
+
+    it("returns user info", function(done){
+      co(function* () {
+        var response = yield model.authenticate(credentials);
+        assert.deepEqual({
+          token: "12345",
+          email: "me@luizbranco.com",
+          projects: ["test"]
+        }, response);
+        done();
+      })();
+    });
+
+  });
+
+});
