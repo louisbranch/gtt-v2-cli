f6edda14b39c2d3b966e7d0f4a28eef8175daafc
Luiz Branco <me@luizbranco.com>
2014-05-04 13:50:57 -0300

    Fix day test

--- test/models/day.js
+++ test/models/day.js
@@ -1,15 +1,28 @@
 var co = require("co");
 var assert = require("assert");
 var nock = require("nock");
+var sinon = require("sinon");
 var model = require("../../api/models/day");
+var clock = require("../../api/helpers/clock");
 
 describe("day model", function(){
+  var DateStub;
+
+  before(function(){
+    sinon.stub(clock, "date", function () {
+      return "2014-04-10";
+    });
+  });
+
+  after(function(){
+    clock.date.restore();
+  });
 
   describe("start", function(){
 
     before(function(){
       nock("http://localhost:8080/")
-        .post("/v1/projects/test/days?date=2014-04-24" +
+        .post("/v1/projects/test/days?date=2014-04-10" +
               "&email=me@luizbranco.com&token=12345")
         .reply(200, {
           date: "2014-04-24"
